4351cb36cb29b0b4eb566d68ae8319b48e7546c1
marc aron alfonso <marc.aron.alfonso21@gmail.com>
2025-02-25 16:32:52 +0800

    tutorial challange

---
+++ js practice 2/tutorialjs2.js
@@ -0,0 +1,12 @@
+
+
+// tutorial challange 1
+let p1 = "Alenere";
+let p2 = "david";
+let pronoun = "she";
+let drink = "coffee";
+
+alert(p1+ ", the friendly neighbor, waved at " +p2 + " as " + pronoun + " walked by " +p2 + "s" + " house. " + p2 + " smiled back and invited " + p1 + " in for a cup of coffe " );
+console.log ("hello world");
+
+
